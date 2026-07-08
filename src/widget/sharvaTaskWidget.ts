ab66f122ab3b42ef9752cff3bb6dbbceb12b27cb
chittalaswamysharavan-8991 <chittalaswamysharavan@gmail.com>
2026-07-08 16:44:07 +0530

    Phase E: version widget resource URI for click diagnostics

--- src/widget/sharvaTaskWidget.ts
+++ src/widget/sharvaTaskWidget.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
 
-export const SHARVATASK_WIDGET_URI = 'ui://widget/sharvatask.html';
+export const SHARVATASK_WIDGET_URI = 'ui://widget/sharvatask-v2-4-clickdiag.html';
 
 export const sharvaTaskWidgetHtml = readFileSync(join(process.cwd(), 'src', 'widget', 'sharvaTaskWidget.html'), 'utf8');
